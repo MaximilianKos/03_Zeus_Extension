ebfc63aa02b3655a3d6d9872c721d6eaedcef479
Maximilian <maximiliankos.x@gmail.com>
2023-03-23 22:04:29 +0100

    working time, added back essential features (css)

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 var getSaldo = function() {
-	var accountElements = document.getElementsByClassName("account-list-element-value account-column");
+	var accountElements = document.getElementsByClassName('account-list-element-value account-column');
 	var data = [];
 	for (var i = 0; i < accountElements.length; i++) {
 		data.push(accountElements[i].textContent);
@@ -11,43 +11,57 @@ var getSaldo = function() {
 }
 
 window.onload = function() {
-	
-
-
 	// Configure the observer to watch for changes to the style attribute
 	observer.observe(workspaceCallbackProcess, { attributes: true });
 }
+
 	// Select the element to observe
-	const workspaceCallbackProcess = document.getElementById("workspaceCallbackProcess");
+	const workspaceCallbackProcess = document.getElementById('workspaceCallbackProcess');
 
 	// Create a new MutationObserver
 	const observer = new MutationObserver((mutations) => {
 	mutations.forEach((mutation) => {
 		// Check if the style attribute was modified
-		if (mutation.attributeName === "style") {console.log("yur2")
-			console.log("yur")
-			getSaldo();
+		if (mutation.attributeName === 'style') {
+			setTimeout(getSaldo(), 20)
 		}
 	});
-	});
+});
 function calculateTime(saldo, difference) {
+	console.log("Saldo: " + saldo)
+	console.log("Difference: " + difference)
+	// Check that the input parameters are in the expected format of 'h.mm'
+	if (!/^\d+\.\d{2}$/.test(saldo) || !/^\d+\.\d{2}$/.test(difference)) {
+	  return 'Invalid input format. Expected format: h.mm';
+	}
+  
 	// Extract hours and minutes from the input strings
 	const [saldoHours, saldoMinutes] = saldo.split('.').map(Number);
 	const [diffHours, diffMinutes] = difference.split('.').map(Number);
   
 	// Calculate the total minutes
-	let totalMinutes = (diffHours * 60 + diffMinutes) - (saldoHours * 60 + saldoMinutes);
+	let SaldoInMinutes = (saldoHours * 60 + saldoMinutes);
+	let DiffInMinutes =  (diffHours * 60 + diffMinutes);
+	
+  
+	// Add the decimal part of the saldo to the total minutes
+	totalMinutes =  DiffInMinutes - SaldoInMinutes
   
 	// Calculate the final hours and minutes
 	const resultHours = Math.floor(totalMinutes / 60);
-	totalMinutes %= 60;
-  
-	// Format the result as a string in the "h.mm" format
-	const result = `${resultHours}.${totalMinutes.toString().padStart(2, '0')}`;
-	return result;
-}
-  
 
+	const resultMinutes = totalMinutes % 60;
+  
+	// Format the result as a string in the 'h.mm' format
+	const hoursString = resultHours.toString();
+	const minutesString = resultMinutes.toString().padStart(2, '0');
+  
+	// Join the hours and minutes strings together with the appropriate formatting
+	const result = `${hoursString}.${minutesString}`;
+	console.log(result)
+	return result;
+  
+}
 
 function displaySaldo(time, data) {
 	if (data[0] !== 'Absent' && data[1] < 0) {
@@ -58,7 +72,7 @@ function displaySaldo(time, data) {
 		// create a new div element with the account-row class
 		const divRow = document.createElement('div');
 		divRow.className = 'account-row';
-		divRow.style.color = "#ff675c"
+		divRow.style.color = '#ff675c'
 
 		// create a new div element with the account-column class
 		const divColumn = document.createElement('div');
@@ -86,10 +100,10 @@ function displaySaldo(time, data) {
 		// append the row div to the li element
 		liElement.appendChild(divRow);
 
-		// get the element with the "account-info-result" class and append the new li element to it
+		// get the element with the 'account-info-result' class and append the new li element to it
 		const accountInfoResult = document.querySelector('.account-list');
 		accountInfoResult.appendChild(liElement);
 	} else {
-		console.log("Absent")
+		console.log('Absent')
 	}
   }
